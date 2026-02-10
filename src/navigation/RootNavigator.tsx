208d53c629775da1f66fccf6b60f63c581a17fa3
wonyeoe <yeowonlee04@gmail.com>
2026-02-11 00:47:39 +0900

    온보딩 완료 여부를 AsyncStorage에 저장

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import OnboardingStack from "./OnboardingStack";
 import WelcomeScreen from "../screens/onboarding/WelcomeScreen";
 
@@ -12,6 +13,16 @@ export type RootStackParamList = {
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const RootNavigator = () => {
+    // 온보딩 완료 상태를 AsyncStorage에 저장하고 Welcome 화면으로 이동
+    const handleOnboardingComplete = async (navigation: any) => {
+		try {
+			await AsyncStorage.setItem("@onboarding_completed", "true");
+			navigation.navigate("Welcome");
+		} catch (error) {
+			console.error("Failed to save onboarding status:", error);
+		}
+	};
+
 	return (
 		<NavigationContainer>
 			<Stack.Navigator
@@ -23,7 +34,7 @@ const RootNavigator = () => {
 				<Stack.Screen name="Onboarding">
 					{(props) => (
 						<OnboardingStack
-							onComplete={() => props.navigation.navigate("Welcome")}
+							onComplete={() => handleOnboardingComplete(props.navigation)}
 						/>
 					)}
 				</Stack.Screen>
